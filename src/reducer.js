63f6035a0e799ae870a96afc4c116e768ca0e796
Maxim <110911222+Maxeroz@users.noreply.github.com>
2024-04-26 23:00:41 +0400

    Cart Decrease

--- src/reducer.js
+++ src/reducer.js
@@ -22,6 +22,19 @@ const reducer = (state, action) => {
         ...state,
         cart: tempCart,
       };
+    case "DECREASE":
+      let tempCart2 = state.cart
+        .map((cartItem) => {
+          if (cartItem.id === action.payload)
+            return { ...cartItem, amount: cartItem.amount-- };
+
+          return cartItem;
+        })
+        .filter((cartItem) => cartItem.amount !== 0);
+      return {
+        ...state,
+        cart: tempCart2,
+      };
     default:
       throw new Error("Unkown action type");
   }
